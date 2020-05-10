e2a248a53a01d580e9083f1994fa482867935033
Dmarcan <marcanp.diego@gmail.com>
2020-05-10 14:15:47 -0400

    starting site

---
+++ naive_bayes/components/Header.js
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+
+const linkStyle = {
+    marginRight : 15
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      color:  'blue',
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+
+    navBar : {
+        color : 'blue'
+    },
+  }));
+  
+
+
+const Header = () => { 
+    const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Naive Bayes
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+export default Header;
---
+++ naive_bayes/components/Layout.js
@@ -0,0 +1,19 @@
+import Header from './Header'
+import Container from '@material-ui/core/Container'
+
+const layoutStyle = {
+    margin : 20, 
+    padding : 20, 
+    // border : '1px solid #DDD'
+}
+
+const Layout  = props => (
+    <div>
+        <Header / >
+            <Container maxWidth="sm">
+                {props.children}
+            </Container>
+    </div>
+)
+
+export default Layout
